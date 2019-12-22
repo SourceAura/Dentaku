23d4f53acf91b52b2d9ae425f8eddd5d1b7a9fd0
Isreal <in.visual7@gmail.com>
2019-12-22 16:54:39 -0500

    ✍

---
+++ dist/precache-manifest.853b6543ef4f721d321b4858b2c2e681.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1c2b23aee698d4b6a04",
+    "url": "/css/app.57842ae3.css"
+  },
+  {
+    "revision": "bb6011893037d80c6505",
+    "url": "/css/chunk-vendors.d79022d0.css"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/fonts/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/fonts/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/fonts/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "d120c85b6eb0549d60483e2980033c15",
+    "url": "/fonts/MaterialIcons-Regular.d120c85b.ttf"
+  },
+  {
+    "revision": "cf9b4d6b9c441f4d1af4a24f7cac13e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84ad5bcce5ebce6bcde2",
+    "url": "/js/about.9d97dc91.js"
+  },
+  {
+    "revision": "b1c2b23aee698d4b6a04",
+    "url": "/js/app.a51db547.js"
+  },
+  {
+    "revision": "bb6011893037d80c6505",
+    "url": "/js/chunk-vendors.d42a55e1.js"
+  },
+  {
+    "revision": "8e781701213f7387e908d5b3b4d138b7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
